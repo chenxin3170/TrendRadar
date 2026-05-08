489c01d7cc1e4f84547679844402dacd7ee3b2a7
盘兮 <516954023@qq.com>
2026-05-08 20:01:35 +0800

    fix: 更新51LA统计SDK的配置参数
    更新了51LA统计SDK的ID和CK参数，并添加了ID标识以便于跟踪

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,8 +56,8 @@ export default defineNuxtConfig({
       script: [
         { src: 'https://www.googletagmanager.com/gtag/js?id=G-FP96X2GXER', async: true },
         { innerHTML: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-FP96X2GXER');", type: 'text/javascript' },
-        { charset: 'UTF-8', src: '//sdk.51.la/js-sdk-pro.min.js' },
-        { innerHTML: 'LA.init({id:"3Ps2xqfhfzK7S9Mn",ck:"3Ps2xqfhfzK7S9Mn"})', type: 'text/javascript' }
+        { charset: 'UTF-8', id: 'LA_COLLECT', src: '//sdk.51.la/js-sdk-pro.min.js' },
+        { innerHTML: 'LA.init({id:"3Ps4XpG1BaKxTj3W",ck:"3Ps4XpG1BaKxTj3W"})', type: 'text/javascript' }
       ]
     }
   },
